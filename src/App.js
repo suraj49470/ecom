f933cf554708ffa3f7515739841407c2d6eccd05
suraj singh <suraj.singh@realtyredefined.com>
2017-05-24 15:31:40 +0530

    props changes

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ class App extends Component {
                        
                       //initialize main conponent's(App) state
                       this.state = {};
+                    
 
            }
      componentDidMount(){
