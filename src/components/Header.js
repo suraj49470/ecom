950d81932ed13e726405108ba5b34e32d2881a1d
suraj singh <suraj.singh@realtyredefined.com>
2017-05-25 15:55:15 +0530

    style addes

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,12 +12,12 @@ export class Header extends React.Component{
                
                       return(
 
-                            <nav className="navbar navbar-inverse navbar-fixed-top">
+                            <nav className="navbar navbar-inverse navbar-fixed-top top-nav">
 							  <div className="container-fluid">
 							    <div className="navbar-header">
-							      <Link className="navbar-brand" to="/">Ecom</Link>
+							      <Link className="navbar-brand top-nav-head" to="/">Ecom</Link>
 							    </div>
-							    <ul className="nav navbar-nav">
+							    <ul className="nav navbar-nav top-nav">
 							      <li><Link to="/men">Men</Link></li>
 							      <li><Link to="/women">women</Link></li>
 							      <li><Link to="/mobile">mobile</Link></li>
